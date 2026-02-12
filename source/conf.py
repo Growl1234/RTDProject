1c94119f3815527e7741b94a9cb16796cd8b1629
SY Wang <growl1234@outlook.com>
2026-02-12 12:42:18 +0800

    Test RTD PR build

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,10 @@ language = 'en'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+
 html_static_path = ['_static']
 html_css_files = ['custom.css']
+
 html_theme_options = {
     'collapse_navigation': False,
     'sticky_navigation': True,
